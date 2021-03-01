d720a423d35147dc6006d50cfece470f936bde34
Shany Danieli <shany.danieli@yale.edu>
2021-03-01 11:47:05 +0200

    add pkg set-up tools

---
+++ python/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup (
+    name = 'DFTile',
+    version='0.0.1',
+    author='Shany Danieli',
+    description='Images tiling tools for Dragonfly Surveys',
+    packages=setuptools.find_packages()
+    )
